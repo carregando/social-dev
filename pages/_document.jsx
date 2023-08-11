37e4c4598692687e47e4abc1e03bfa6c990537fd
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-08-10 20:46:54 -0400

    Instalado e configurado styled components

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ class NewDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Social Dev</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
